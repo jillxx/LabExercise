4ad03e596604427d05b9ac70a3edbdd44a4c3a7e
jillxx <jiaoxu84@gmail.com>
2018-04-24 11:50:09 -0400

    first commit

---
+++ src/exercise1.java
@@ -0,0 +1,11 @@
+import java.util. Scanner;
+public class exercise1 {
+    public static void main(String []args){
+        String userInput;
+      
+         Scanner reader = new Scanner(System.in);
+         System.out.println("Enter some text:");
+         userInput = reader.nextLine();
+         System.out.println(userInput); 
+    }     
+}
